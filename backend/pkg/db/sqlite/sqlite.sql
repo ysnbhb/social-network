fba2a1575e82ec91637f036216cb78cc20f6dc06
saljaoui <soufian2022ff@gmail.com>
2025-02-06 18:16:43 +0100

    add app.db

--- backend/pkg/db/sqlite/sqlite.sql
+++ backend/pkg/db/sqlite/sqlite.sql
@@ -1,16 +1,17 @@
 -- Users Table
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT NOT NULL UNIQUE CHECK (email LIKE '%@%.%'),
+    email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     date_of_birth DATE NOT NULL,
-    avatar_url TEXT, -- Optional
-    nickname TEXT UNIQUE, -- Optional
-    about_me TEXT, -- Optional
-    profile_type TEXT NOT NULL DEFAULT 'private' CHECK (profile_type IN ('public', 'private')),
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    avatar_url TEXT,
+    nickname TEXT UNIQUE,
+    about_me TEXT,
+    profile_type TEXT NOT NULL DEFAULT 'private',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status TEXT DEFAULT 'offline'
 );
 
 -- Followers Table
@@ -18,10 +19,10 @@ CREATE TABLE followers (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     follower_id INTEGER NOT NULL,
     following_id INTEGER NOT NULL,
-    status TEXT NOT NULL DEFAULT 'pending' CHECK (status IN ('accepted', 'rejected', 'pending')),
+    status TEXT NOT NULL DEFAULT 'pending',
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (follower_id) REFERENCES users(id),
-    FOREIGN KEY (following_id) REFERENCES users(id),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(following_id) REFERENCES users(id),
     UNIQUE(follower_id, following_id)
 );
 
@@ -29,17 +30,20 @@ CREATE TABLE followers (
 CREATE TABLE card (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
+    group_id INTEGER, -- NULL means user posted at home
     content TEXT NOT NULL,
+    image_url TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(group_id) REFERENCES groups(id)
 );
 
 -- Posts Table
 CREATE TABLE posts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
     card_id INTEGER NOT NULL,
-    FOREIGN KEY (card_id) REFERENCES card(id)
+    privacy TEXT DEFAULT 'public', -- can be public, private, almost_private
+    FOREIGN KEY(card_id) REFERENCES card(id)
 );
 
 -- Comments Table
@@ -47,10 +51,8 @@ CREATE TABLE comments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     card_id INTEGER NOT NULL,
     target_id INTEGER NOT NULL,
-    content TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (card_id) REFERENCES card(id),
-    FOREIGN KEY (target_id) REFERENCES card(id)
+    FOREIGN KEY(card_id) REFERENCES card(id),
+    FOREIGN KEY(target_id) REFERENCES card(id)
 );
 
 -- Groups Table
@@ -60,7 +62,7 @@ CREATE TABLE groups (
     description TEXT NOT NULL,
     creator_id INTEGER NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (creator_id) REFERENCES users(id)
+    FOREIGN KEY(creator_id) REFERENCES users(id)
 );
 
 -- Group Members Table
@@ -69,8 +71,8 @@ CREATE TABLE group_members (
     group_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     joined_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (group_id) REFERENCES groups(id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY(group_id) REFERENCES groups(id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
     UNIQUE(group_id, user_id)
 );
 
@@ -79,10 +81,10 @@ CREATE TABLE group_invitations (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     group_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
-    status TEXT NOT NULL DEFAULT 'pending' CHECK (status IN ('accepted', 'rejected', 'pending')),
+    status TEXT NOT NULL DEFAULT 'pending',
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (group_id) REFERENCES groups(id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY(group_id) REFERENCES groups(id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
     UNIQUE(group_id, user_id)
 );
 
@@ -90,11 +92,13 @@ CREATE TABLE group_invitations (
 CREATE TABLE events (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     group_id INTEGER NOT NULL,
+    creator_id INTEGER NOT NULL,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
     day_time DATETIME NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (group_id) REFERENCES groups(id)
+    FOREIGN KEY(group_id) REFERENCES groups(id),
+    FOREIGN KEY(creator_id) REFERENCES users(id)
 );
 
 -- Event Responses Table
@@ -102,21 +106,21 @@ CREATE TABLE event_responses (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     event_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
-    response TEXT NOT NULL CHECK (response IN ('going', 'not going')),
-    FOREIGN KEY (event_id) REFERENCES events(id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    response TEXT NOT NULL, -- going or not going
+    FOREIGN KEY(event_id) REFERENCES events(id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
     UNIQUE(event_id, user_id)
 );
 
--- Chats Table (Private Messages)
+-- Chats Table
 CREATE TABLE chats (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     sender_id INTEGER NOT NULL,
     recipient_id INTEGER NOT NULL,
     message TEXT NOT NULL,
     sent_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (sender_id) REFERENCES users(id),
-    FOREIGN KEY (recipient_id) REFERENCES users(id)
+    FOREIGN KEY(sender_id) REFERENCES users(id),
+    FOREIGN KEY(recipient_id) REFERENCES users(id)
 );
 
 -- Group Chats Table
@@ -126,27 +130,28 @@ CREATE TABLE group_chats (
     user_id INTEGER NOT NULL,
     message TEXT NOT NULL,
     sent_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (group_id) REFERENCES groups(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY(group_id) REFERENCES groups(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 -- Notifications Table
 CREATE TABLE notifications (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
-    type TEXT NOT NULL, -- e.g., 'follow_request', 'group_invitation', etc.
+    type TEXT NOT NULL,
     details TEXT NOT NULL,
-    read_status TEXT NOT NULL DEFAULT 'unread' CHECK (read_status IN ('read', 'unread')),
+    read_status TEXT NOT NULL DEFAULT 'unread',
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 -- Sessions Table
 CREATE TABLE sessions (
     session_id TEXT PRIMARY KEY,
     user_id INTEGER NOT NULL,
+    user_uuid TEXT NOT NULL,
     expires_at DATETIME NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 -- Likes Table
@@ -154,9 +159,9 @@ CREATE TABLE likes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     card_id INTEGER NOT NULL,
-    reaction_type TEXT NOT NULL CHECK (reaction_type IN ('like', 'dislike')),
+    reaction_type TEXT NOT NULL, -- like or dislike
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (card_id) REFERENCES card(id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(card_id) REFERENCES card(id),
     UNIQUE(user_id, card_id)
 );
