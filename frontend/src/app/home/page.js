bc433ca8969ccd10b0571082474442c809e737a7
omrharbi <rharbu.omar@gmail.com>
2025-02-20 11:08:16 +0100

    done
    Former-commit-id: 537a066bec71389d4da0c8861d24460a2a19e878

--- frontend/src/app/home/page.js
+++ frontend/src/app/home/page.js
@@ -1,11 +1,10 @@
 "use client"
 
-import { useState } from 'react';
 import styles from "./page.module.css"
 import notification from "../../component/notification.svg"
 import image from "../../component/user.jpg"
 import Image from 'next/image';
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 const dislike = "M240-840h440v520L400-40l-50-50q-7-7-11.5-19t-4.5-23v-14l44-174H120q-32 0-56-24t-24-56v-80q0-7 2-15t4-15l120-282q9-20 30-34t44-14Zm360 80H240L120-480v80h360l-54 220 174-174v-406Zm0 406v-406 406Zm80 34v-80h120v-360H680v-80h200v520H680Z"
 const like = "M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z"
 const comment = "M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"
@@ -34,8 +33,8 @@ export default function Home() {
     ]
     const posts = [
         { name: "user1", title: "Title1", time: "10:50", content: "Paragraph 1" },
-        // { name: "user2", title: "Title2", time: "11:50", content: "Paragraph 2" },
-        // { name: "user3", title: "Title3", time: "12:50", content: "Paragraph 3" },
+        { name: "user2", title: "Title2", time: "11:50", content: "Paragraph 2" },
+        { name: "user3", title: "Title3", time: "12:50", content: "Paragraph 3" },
         // { name: "user4", title: "Title4", time: "13:50", content: "Paragraph 4" },
         // { name: "user5", title: "Title5", time: "14:50", content: "Paragraph 5" },
         // { name: "user6", title: "Title6", time: "15:50", content: "Paragraph 6" },
