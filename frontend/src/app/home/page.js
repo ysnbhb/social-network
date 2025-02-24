996f5b84039964b59ea56ec472a1534653368fe6
saljaoui <soufian2022ff@gmail.com>
2025-02-24 16:46:07 +0000

    animation to navbar
    Former-commit-id: 62afbfcb7150ae0c8bf5a11d7e11c3dffd012add

--- frontend/src/app/home/page.js
+++ frontend/src/app/home/page.js
@@ -1,17 +1,11 @@
-import Navbar from '../../components/navbar.js';
 import ProfileSide from '../../components/profileSide.js';
 import HomeFeed from '../../components/homeFeed.js';
 import ActivitySide from '../../components/activitySide.js';
 // import profileSide from '../../components/profileSide.js'; // Correct the import statement to match your file structure
 
-import '../home/globals.css';
-
-
 export default function Home() {
   return (
-    <div>
-      <Navbar /> {/* Use Navbar as a JSX element */}
-      {/* Other content for the home page */}
+    
 
       <main className="main-content">
       <ProfileSide />
@@ -19,6 +13,6 @@ export default function Home() {
       <ActivitySide />
       </main>
 
-    </div>
+    
   );
 }
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Link from "next/link";
+import './globals.css';
 
 export default function Home() {
   return (
