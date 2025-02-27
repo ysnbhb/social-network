9c8f85600f6963c1bcd2c412905da21bed243506
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-02-27 12:43:06 +0000

    look good
    Former-commit-id: 76424625c05035dae401af1d07c5e76b08fc0d78

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
       {
         source: "/api/:path*",
         destination: "http://localhost:8080/api/:path*",
+      },{
+        source: "/uploads/:path*",
+        destination: "http://localhost:8080/uploads/:path*",
       },
     ];
   },
--- frontend/src/components/postComp.js
+++ frontend/src/components/postComp.js
@@ -11,13 +11,14 @@ export function PostCompte({post}) {
                 <div className="post-author">
                     <div className="avatar"></div>
                     <div>
-                        <h4>{nickName}</h4>
+                        <h4>{`${firstName} ${lastName}`}</h4>
+                        <p className="text-muted">@{nickName}</p>
                         <p className="text-muted">{createdAt}</p>
                     </div>
                 </div>
             </div>
             <p>{content}</p>
-            {imageUrl && <img src={`http://localhost:8080${imageUrl}`} alt="Post" style={{ width: "100%", height: "auto" }} />}
+            {imageUrl && <img src={`${imageUrl}`} alt="Post" style={{ width: "100%", height: "auto" }} />}
             {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> */}
             <div className="post-actions">
                 <div className={"like" + (like ? " active" : "")} onClick={() => setLike(!like)}>
