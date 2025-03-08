486a255453006f071be837d43694c2cb9b626f7f
omrharbi <rharbu.omar@gmail.com>
2025-03-08 12:00:17 +0000

    done

--- frontend/src/app/hooks/useFollowing.js
+++ frontend/src/app/hooks/useFollowing.js
@@ -1,24 +1,27 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export default  function useFollowing(params){
-    const [follow,setFollow] =useState([])
-    const [err,seterr] =useState([])
-    const following=async ()=>{
-        try {
-            const response=await fetch(`/api/userfollowing?username${params}`,
-            { credentials:"include"} )
-            let data = await response.json()
-            if (response.ok){
-                setFollow(data|| [])
-            }else{
-                seterr(data)
-            }
-        } catch (error) {
-            seterr(data)
-        }
+export default function useFollowing( ) {
+  const [Following, setFollowing] = useState([]);
+  const [error, setError] = useState(null);
+  const dataFollowing = async () => {
+    try {
+      const response = await fetch(`/api/userfollowing`, {
+        credentials: "include",
+      });
+      let data = await response.json();
+
+      if (response.ok) {
+        setFollowing(data || []);
+      } else {
+        setError(data);
+      }
+    } catch (error) {
+      setError(error.message);
     }
-    useEffect(()=>{
-        following
-    },[params])
-return [follow,err]
+  };
+  useEffect(() => {
+    dataFollowing();
+  }, [ ]);
+
+  return [Following, error];
 }
