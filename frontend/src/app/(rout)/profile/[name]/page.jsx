9150ef9187be74b334c028aff7d25feb9d663516
omrharbi <rharbu.omar@gmail.com>
2025-03-13 11:30:25 +0000

    done

--- frontend/src/app/(rout)/profile/[name]/page.jsx
+++ frontend/src/app/(rout)/profile/[name]/page.jsx
@@ -2,7 +2,6 @@
 import React, { use, useEffect, useState } from "react";
 import Image from "next/image";
 import style from "./profile.module.css";
-import styles from "./updateProfile.module.css";
 import bag from "@/components/images/pxfuel.jpg";
 import { PostCompte } from "../../../../components/postComp.js";
 import useGetProfile from "@/app/hooks/useGetProfile";
@@ -13,6 +12,8 @@ import PopUpError from "@/components/popupError";
 import { useParams, useRouter } from "next/navigation";
 import { API_URL } from "@/components/api";
 import useEditProfile from "@/app/hooks/useEditProfile";
+import Link from "next/link";
+
 export default function Profile({ params }) {
   const [showPopup, setShowPopup] = useState(false);
   const router = useRouter();
@@ -107,11 +108,11 @@ export default function Profile({ params }) {
                   <div className={style.buttonContainer}>
                     {isOwnProfile ? (
                       <div>
-                        <button
-                          onClick={togglePopup}
+                        <Link
+                          href={{ pathname: `/settings/${nickName}` }}
                           className={style.moreButton}>
                           Edit Profile
-                        </button>
+                        </Link>
                       </div>
                     ) : (
                       <div>
@@ -191,158 +192,6 @@ export default function Profile({ params }) {
               </div>
             </div>
           )}
-        </div>
-      )}
-    </div>
-  );
-}
-
-export function Updateprofile({ data, show, setShowPopup }) {
-  const router = useRouter();
-  const [profileType, setProfileType] = useState("Public");
-  const params = useParams();
-  const name = params.name;
-  const [profile, setProfile] = useState({ username: name });
-  const updatedProfile = useEditProfile(profile);
-
-  const handleRadioChange = (event) => {
-    setProfileType(event.target.value);
-  };
-  const handleSignUp = async (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    formData.append("profile_type", profileType);
-    const formObject = {};
-    for (const [key, value] of formData.entries()) {
-      formObject[key] = value;
-    }
-    setProfile(formObject);
-    router.push(`/profile/${formObject.nickName}`);
-   };
-  const change = () => {
-     
-    router.push(`/profile/${data.nickName}`);
-    router.refresh();
-  };
-  const togglePopup = () => {
-    setShowPopup(!show);
-  };
-
-  return (
-    <div>
-      {show && (
-        <div className={styles.update}>
-          <div className={styles.container}>
-            <div className={styles.header}>
-              <h2>Account Settings</h2>
-              <button className={styles.closeButton} onClick={togglePopup}>
-                ×
-              </button>
-            </div>
-
-            <h3>Your Avatar</h3>
-            <div className={styles.avatarContainer}>
-              <span className={styles["Circle-avart"]}>
-                <img
-                  src={`${API_URL}${data.avatarUrl}`}
-                  className={`${styles["avatarContainer-profile"]} ${style.avatarContainer}`}
-                  srcSet=""
-                  alt="User Avatar"
-                  layout="fill"
-                  objectfit="cover"
-                />
-              </span>
-              <p className={styles.avatarText}>
-                Avatar help your teammates recognize you in Social Network .
-              </p>
-            </div>
-
-            <hr className={styles.hr} />
-            <form onSubmit={handleSignUp}>
-              <div className={styles.formRow}>
-                <div className={styles.formGroup}>
-                  <label className={styles.label}>First Name</label>
-                  <input
-                    className={styles.input}
-                    type="text"
-                    defaultValue={data.firstName}
-                    name="firstName"
-                  />
-                </div>
-                <div className={styles.formGroup}>
-                  <label className={styles.label}>Last Name</label>
-                  <input
-                    className={styles.input}
-                    type="text"
-                    defaultValue={data.lastName}
-                    name="lastName"
-                  />
-                </div>
-              </div>
-
-              <div className={styles.formRow}>
-                <div className={styles.formGroup}>
-                  <label className={styles.label}>User Name</label>
-                  <input
-                    className={styles.input}
-                    type="text"
-                    defaultValue={data.nickName}
-                    name="nickName"
-                  />
-                </div>
-                <div className={styles.formGroup}>
-                  <label className={styles.label}>Email</label>
-                  <input
-                    className={styles.input}
-                    type="text"
-                    defaultValue={data.email}
-                    name="email"
-                  />
-                </div>
-              </div>
-
-              <div className={styles["formRow-radio"]}>
-                <label className={styles.label}>Type Profile</label>
-                <div className={styles.formradio}>
-                  <label className={styles.label}>Public</label>
-                  <input
-                    className={styles.radio}
-                    type="radio"
-                    name="profile_type"
-                    value="Public"
-                    checked={profileType === "Public"}
-                    onChange={handleRadioChange}
-                  />
-
-                  <label className={styles.label}>Private</label>
-                  <input
-                    className={styles.radio}
-                    type="radio"
-                    name="profile_type"
-                    value="Private"
-                    checked={profileType === "Private"}
-                    onChange={handleRadioChange}
-                  />
-                </div>
-              </div>
-
-              <div className={styles.formGroup}>
-                <label className={styles.label}>About Me</label>
-                <textarea
-                  className={styles.textarea}
-                  rows="4"
-                  name="aboutMe"
-                  defaultValue={data.aboutMe}
-                />
-              </div>
-
-              {/* Update profile button */}
-              <button className={styles.updateButton} onClick={change}>
-                Update Profile
-              </button>
-            </form>
-          </div>
         </div>
       )}
     </div>
