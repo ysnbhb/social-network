4543523f26e6a6729e0596417a8d506cce698ee9
omrharbi <rharbu.omar@gmail.com>
2025-03-18 15:48:44 +0000

    fix error following

--- backend/pkg/db/repositories/user_profile_repo.go
+++ backend/pkg/db/repositories/user_profile_repo.go
@@ -177,11 +177,7 @@ func GetUserFollowing(current_userId int, my_userid int) (friend []models.Unfoll
 	JOIN followers f ON u.id = f.following_id
 	WHERE 
     f.follower_id = $2 
-<<<<<<< HEAD
 	AND u.id!=$2
-=======
- 	AND u.id!=$1
->>>>>>> omrharbi
     AND 
     (
         ($1 = $2) 
