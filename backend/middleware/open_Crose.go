a851771cf3747e0998a9fc0b763f5f4a282c3087
omrharbi <rharbu.omar@gmail.com>
2025-04-10 11:15:39 +0100

    done

--- backend/middleware/open_Crose.go
+++ backend/middleware/open_Crose.go
@@ -5,18 +5,32 @@ import "net/http"
 func WithCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
-		allowedOrigin := "http://localhost:3000"
-		if origin == allowedOrigin {
-			w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-		}
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT , DELETE ")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
+        // Define the list of allowed origins
+        allowedOrigins := []string{"http://localhost:3000", "http://10.1.6.8:3000"}
+
+        // Check if the origin is allowed
+        isAllowed := false
+        for _, allowedOrigin := range allowedOrigins {
+            if origin == allowedOrigin {
+                isAllowed = true
+                break
+            }
+        }
+
+        // If the origin is allowed, set the CORS headers
+        if isAllowed {
+            w.Header().Set("Access-Control-Allow-Origin", origin)
+            w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+            w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+        }
+
+        // Handle preflight requests
+        if r.Method == http.MethodOptions {
+            w.WriteHeader(http.StatusOK)
+            return
+        }
 		next.ServeHTTP(w, r)
 	})
 }
