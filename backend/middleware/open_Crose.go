97e68ee8c03d93abee6dcd09de7b6179855f6d63
YassineHamdouni <yassinehamdouni023@gmail.com>
2025-04-11 10:49:06 +0100

    edit crose

--- backend/middleware/open_Crose.go
+++ backend/middleware/open_Crose.go
@@ -7,22 +7,18 @@ import "net/http"
 func WithCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
-
-		allowedOrigins := []string{"http://localhost:3000", "http://10.1.6.8:3000"}
-
-		// Check if the origin is allowed
-		isAllowed := slices.Contains(allowedOrigins, origin)
-		if isAllowed {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		allowedOrigin := "http://localhost:3000"
+		if origin == allowedOrigin {
+			w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
 		}
- 		if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT , DELETE ")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
-
 		next.ServeHTTP(w, r)
 	})
 }
