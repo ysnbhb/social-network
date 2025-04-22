ce239a2e03543fd3927d169bd5577172756b8c5c
yassine bahbib <yassine.bahbib@usmba.ac.ma>
2025-04-22 18:47:37 +0100

    good

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,4 +1,3 @@
+export const API_URL = process.env.BACKEND_URL || "http://localhost:8080";
 
-export const  API_URL = 'http://localhost:8080';
-
-export const  API_URL_WS = 'ws://localhost:8080';
+export const API_URL_WS = process.env.BACKEND_WS || "ws://localhost:8080";
